2fc95b09f3062b44e7578d3006253bdea2285671
Felipe Cechin <ficechin@hotmail.com>
2016-09-02 16:03:57 -0300

    Arquivos de base do sistema adicionados.

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Microsoft</title>
+        <link rel="icon" href="imagens/favicon.ico">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="keywords" content="Microsot, informática, computação">
+        <meta name="description" content="Site semelhante ao da Microsoft">
+        <link rel="stylesheet" href="css/estilo.css">
+        <script src="js/slideshow.js"></script>
+    </head>
+    <body>
+        <table width="100%">
+            <tr id="menu">
+                <td>
+                    <table width="80%" align="center">
+                        <tr>
+                            <td>
+                                <img src="imagens/microsoft.png" alt="Microsoft" id="imgMicrosoft">
+                            </td>
+                            <td width="10%" class="colunaMenu">
+                                <a href="#">
+                                    <table align="center"> 
+                                        <tr> 
+                                            <td> 
+                                                Loja 
+                                            </td> 
+                                            <td> 
+                                                <img src="imagens/felchaprabaixo.jpg" class="imgFlecha">
+                                            </td> 
+                                        </tr>
+                                    </table>
+                                </a>
+                            </td>
+                            <td width="10%" class="colunaMenu">
+                                <a href="#">
+                                    <table align="center"> 
+                                        <tr> 
+                                            <td> 
+                                                Produtos 
+                                            </td> 
+                                            <td> 
+                                                <img src="imagens/felchaprabaixo.jpg" class="imgFlecha">
+                                            </td> 
+                                        </tr>
+                                    </table>
+                                </a>
+                            </td>
+                            <td width="10%" class="colunaMenu">
+                                <a href="#">Suporte</a>
+                            </td>
+                            <td width="40%">
+                            </td>
+                            <td width="11%" class="colunaMenu">
+                                <table id="tabelaPesquisar">
+                                    <tr> 
+                                        <td>
+                                            <input type="text" placeholder="Pesquisar no Microsoft.com" id="campoPesquisar">
+                                        </td>
+                                        <td>
+                                            <a href="#">
+                                                <img src="imagens/search.png" id="imgSearch">
+                                            </a>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </td>
+                            <td class="colunaMenu" style="text-align: center;">
+                                <a href="#">
+                                    <table align="center"> 
+                                        <tr> 
+                                            <td>
+                                                <img src="imagens/carrinho.png" id="imgCarrinho">
+                                            </td>
+                                            <td>
+                                                0
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </a>
+
+                            </td>
+                            <td>
+                                <a href="#" class="colunaMenu">Entrar</a>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr id="imagens" width="100%">
+                <td>
+                    <div id="slideShowImages" width="100%">
+                        <img src="imagens/imagem1.jpg" alt="Slide 1" width="100%">
+                        <img src="imagens/imagem2.jpg" alt="Slide 2" width="100%">
+                    </div>  
+                </td>
+            </tr>
+            <tr id="itens">
+
+            </tr>
+            <tr id="imagem">
+
+            </tr>
+            <tr id="itens2">
+
+            </tr>
+            <tr id="seguir">
+
+            </tr>
+            <tr id="menuinferior">
+
+            </tr>
+        </table>
+    </body>
+</html>
